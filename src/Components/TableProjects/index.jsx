fad62e9c294a302a26dd91ac36210de313c57c73
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-05-27 19:52:26 -0300

    feat(TableProjects): creating component table

---
+++ src/Components/TableProjects/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TableProjects = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th> 
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr>
+            <td></td>
+          </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default TableProjects;
