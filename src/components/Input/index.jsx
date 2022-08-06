191ec9392caf054a906b68ac8fe09e8056ec22d8
FernandoMos92 <fernandomosp@gmail.com>
2022-08-06 16:24:19 -0300

    :sparkles: Criado o input de maneira generica que pode ser usado em mais de um local

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SInput = styled.input`
+  font-size:  18px;
+  padding: 5px;
+
+  @media screen and (max-width:  414px) {
+      border: none;
+      background-color:  transparent;
+      border-bottom: 2px groove #ffffff ; 
+      margin-bottom:  15px;
+      font-weight:  600;
+      padding-left:  1.8rem;
+      color:  #141a29;
+      width:  80%;
+      outline: none;
+      z-index:  99;
+
+      &::placeholder {
+        color: #141a29b5;
+      }
+  }
+`;
+
+export default function Input(props) {
+  return (
+    <SInput 
+      type="text"  {...props}
+    />
+  )
+}
--- src/page/Login/index.jsx
+++ src/page/Login/index.jsx
@@ -1,31 +1,67 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
+import Input from '../../components/Input';
 
-const LoginUser = styled.form`
-  box-shadow:  0px 0px 2px #F2F2F2;
-  border-radius:  .37rem;
-  position: absolute;
-  height: 80vh;
-  width: 60vh;
-  position: absolute;
-  top:  6.25rem;
-  left:  34.37rem;
+const LoginUser = styled.div`
+  color:  #F2F2F2;
+  font-size:  24px;
+  text-align: center;
+  position: relative;
+  height:  100vh;
+  width:  100%;
+
+  .login__subTitle {
+    color: #FFB800;
+  }
+  .login__form-user {
+    background-color: #f2f2f276;
+    border-radius:  .37rem;
+    position:  absolute;  
+
+    .login__container-input {
+      bottom:  0;
+      display: flex;
+      height: 60%;
+      flex-flow:  column nowrap;
+      position: absolute;
+      width:  100%;
+      align-items: center;
+    }
+  }
 
   // Regras para mobile até 414px - iPhone
   @media screen and (max-width: 414px) {
-    
+    .login__form-user {
+      left: 3.1rem;
+      height:  16rem;
+      top:  18.25rem;
+      width:  20rem;
+    }
   }
 
   // Regras para monitores maiores que 1920px
   @media screen and (min-width: 1921px) {
-    left:  50rem;
+    left: 50rem;
   }
 `;
 
 export default function Login() {
   return (
     <LoginUser>
-      Login
+      <h1 className="login__mainTitle">Feeder</h1>
+      <h2 className="login__subTitle">Project</h2>
+
+      <form className="login__form-user">
+        <div className="login__container-input">
+          <Input 
+            placeholder="Email or User"
+          />
+          <Input 
+            placeholder="Password"
+            type="password"
+          />
+        </div>
+      </form>
     </LoginUser>
-  )
+  );
 }
