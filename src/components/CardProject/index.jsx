6289dafb86143ec108f03b4ade43aa81f39b3d3b
FernandoMos92 <fernandomosp@gmail.com>
2022-08-10 00:14:55 -0300

    :sparkles: feat: Card pronto, ainda tem alguns detalhes a melhorar e codigo a refatorar

--- src/components/CardProject/index.jsx
+++ src/components/CardProject/index.jsx
@@ -1,34 +1,87 @@
-import React, { useState } from 'react'
-import styled from 'styled-components';
-import { MdOutlineKeyboardArrowUp, MdOutlineKeyboardArrowDown } from 'react-icons/md';
-import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import React, { useState } from 'react';
+import {
+  MdOutlineKeyboardArrowUp,
+  MdOutlineKeyboardArrowDown,
+} from 'react-icons/md';
+import { AiFillStar, AiOutlineStar, AiOutlineLink } from 'react-icons/ai';
+import { BsFillTrash2Fill, BsPencilFill } from 'react-icons/bs';
 import { Card } from '../../style/Card';
 
 export default function CardProject() {
-
   const [drop, setDrop] = useState(true);
 
   return (
-    <Card >
-      <section className='container__feedback-header'>
-        Titulo do projeto
-        <section className='container__feedback'>
-          <AiFillStar  className='card__icon-starFill'/>
-          <AiFillStar  className='card__icon-starFill'/>
-          <AiOutlineStar  className='card__icon-starOutline'/>
-          <AiOutlineStar  className='card__icon-starOutline'/>
-          <AiOutlineStar  className='card__icon-starOutline'/>
+    <Card height={drop ? '5vh' : '40vh'}>
+      <header className="container__feedback-header">
+        <h3 className="contianer__feedback-title">Titulo do projeto</h3>
+        <section className="container__feedback">
+          <AiFillStar />
+          <AiFillStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
         </section>
-        {
-          drop ? 
-          <MdOutlineKeyboardArrowDown 
-            className='card__icon-arrowDown'
-            onClick={ () => setDrop(!drop)}
+        {drop ? (
+          <MdOutlineKeyboardArrowDown
+            className="card__icon-arrowDown"
+            onClick={() => setDrop(!drop)}
           />
-          : <MdOutlineKeyboardArrowUp  onClick={ () => setDrop(!drop)}/>
-        }
-      </section>
-
+        ) : (
+          <MdOutlineKeyboardArrowUp
+            className="card__icon-arrowDown"
+            onClick={() => setDrop(!drop)}
+          />
+        )}
+      </header>
+      {!drop && (
+        <main className="container__feedback-main">
+          <img
+            src="https://i.pinimg.com/originals/0a/4d/cb/0a4dcb92fa2d3c601b58d72720d6bec4.jpg"
+            alt="test"
+            className="main__image-project"
+          />
+          <label 
+            className="main__link-label"
+            htmlFor="main__link-project"
+          >
+            Link to project
+            <a
+              className="main__link-project"
+              href="https://www.teste.com.br"
+            >Click here <AiOutlineLink /></a>
+          </label>
+          <label 
+            className="main__description-label"
+            htmlFor="main__descripton-text"
+          >
+            Description Project
+            <p className="main__descripton-text"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit corrupti ducimus repellendus, laboriosam sapiente excepturi fugiat totam atque reprehenderit, adipisci numquam culpa nihil dicta veritatis. Voluptatum perspiciatis quidem modi aliquid?</p>
+          </label>
+          <label
+            className="main__stacks" 
+            htmlFor=""
+          >
+            <p>Stacks</p>
+            <ul>
+              <li>React.JS</li>
+              <li>SasS</li>
+              <li>Jest</li>
+              <li>Jest</li>
+              <li>Jest</li>
+              <li>Jest</li>
+              <li>Jest</li>
+            </ul>
+          </label>
+          <section className="main__icon-crud">
+            <abbr title="Delete project">
+              <BsFillTrash2Fill className='main__icon-crud-trash'/>
+            </abbr>
+            <abbr title="Edit project">
+              <BsPencilFill className='main__icon-crud-edit' />
+            </abbr>
+          </section>
+        </main>
+      )}
     </Card>
-  )
+  );
 }
